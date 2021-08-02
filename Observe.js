313e3853d55ecb5d93fcd5b7e89bd582df08700f
Dylan Liu <dylan.liu237@gmail.com>
2021-08-02 18:30:28 +0800

    feat: watcher

--- Observe.js
+++ Observe.js
@@ -1,3 +1,39 @@
+
+/* 每個 $data 內部屬性都是都會對應一個 watcher */
+class Watcher {
+  constructor (expr, vm, viewUpdateCallback) {
+    this.expr = expr // 儲存資料位置，可能是 'someData.foo'，訪問資料時使用 getValue 函數傳進 expr 找到對應的值 ( vm.$data.someData.foo )
+    this.vm = vm
+    this.viewUpdateCallback = viewUpdateCallback
+    this.oldValue = this.getOldValue(expr, vm) // 2. 初始化 watcher 時就會觸發這個函數
+  }
+  getOldValue(expr, vm) {
+    Dependency.target = this // 3. 將 this(watcher instance) 作為靜態屬性暫存在 Dependency.target 中
+    const oldValue = compileUtil.getValue(expr, vm) // 4. 這裡執行結束後，隨即觸發 observer 內 defineProperty API 中的 get() 函數。因為 vm.$data 內所有的資料都被Object.defineProperty 劫持了，只要針對內部資料"進行訪問"都會觸發get)
+    Dependency.target = null // 6. watcher 已經在 dep 中，此時便可以清空暫存，到此已完成訂閱，接著則是被動等著被監聽的值被改變 (observer 中的 set()函數)
+    return oldValue
+  }
+  update() {
+    const newValue = compileUtil.getValue(this.expr, this.vm)
+    if (this.oldValue !== newValue) {
+      this.oldValue = newValue
+      this.viewUpdateCallback(newValue)
+    }
+  }
+}
+
+/* 依賴收集器, 儲存 watcher */
+class Dependency {
+  constructor () {
+    this.subscribes = []
+  }
+  add(watcher) {
+    this.subscribes.push(watcher)
+  }
+  notify() {
+    this.subscribes.forEach(watcher => watcher.update())
+  }
+}
 class Observer {
   constructor (data) {
     this.observe(data)
@@ -12,10 +48,13 @@ class Observer {
   defineReactive(obj, key, value) {
     this.observe(value) // 遞迴劫持值為物件的情況
 
+    const dep = new Dependency() // 每個資料都會掛載一個對應的 dep 來存放它的 watcher 實例
+
     Object.defineProperty(obj, key, {
       configurable: false,
       enumerable: true,
       get() {
+        Dependency.target && dep.add(Dependency.target) // 5. 由於第三步驟時儲存了 watcher 的實例在 Dependency.target，到這裏自然會將該 watcher 存進 Dependency 陣列里
         return value
       },
       set: (newValue) => {
@@ -23,6 +62,7 @@ class Observer {
         if (newValue !== value) {
           value = newValue
         }
+        dep.notify() // 7. 未來當資料產生改變時透過 Dependency 的 notify 方法通知被儲存在 dep 中的 watcher,  watcher 則呼叫自己的 update 函數，update 函數則觸發第一步綁定的更新畫面callback
       }
     })
   }
--- Vue.js
+++ Vue.js
@@ -3,10 +3,17 @@ const compileStrategies = {
   text(node, expr, vm) {
     const value = compileUtil.getValue(expr, vm)
     updater.text(node, value)
+    // 1. 創建訂閱者 (watcher)，綁定資料對應的更新畫面callback (後續流程至 watcher 查看)
+    new Watcher(expr, vm, (newValue) => {
+      updater.text(node, newValue)
+    })
   },
   html(node, expr, vm) {
     const value = compileUtil.getValue(expr, vm)
     updater.html(node, value)
+    new Watcher(expr, vm, (newValue) => {
+      updater.html(node, newValue)
+    })
   },
   model(node, expr, vm) {
     const value = compileUtil.getValue(expr, vm)
@@ -14,6 +21,9 @@ const compileStrategies = {
     node.addEventListener('input', (e) => {
       const newValue = e.target.value;
       compileUtil.setValue(expr, vm, newValue)
+    })
+    new Watcher(expr, vm, (newValue) => {
+      updater.model(node, newValue)
     })
   },
   on(node, expr, vm, event) {
@@ -25,6 +35,9 @@ const compileStrategies = {
   bind(node, expr, vm, attr) {
     const value = compileUtil.getValue(expr, vm)
     updater.bind(node, value, attr)
+    new Watcher(expr, vm, (newValue) => {
+      updater.bind(node, newValue)
+    })
   }
 }
 
@@ -60,6 +73,9 @@ const compileUtil = {
   isElementNode(node) {
     return node.nodeType === 1
   },
+  isMustacheSyntax(node) {
+    return node?.textContent?.includes('{{')
+  },
   /* v-text -> text */
   getStrategyKey(attr) {
     return attr?.replace('v-', '') || ''
@@ -78,12 +94,12 @@ const compileUtil = {
       return acc[cur];
     }, vm.$data)
   },
-  /* 取得 {{  }} 內資料 */
-  getMustacheValue(expr, vm) {
+  /* 取得 {{  }} 內字串 */
+  getMustacheExpr(mustache) {
     const REGEXP = /\{\{(.+?)\}\}/g
-    return expr.replace(REGEXP, (...args) => {
-      const content = args[1].trim()
-      return compileUtil.getValue(content, vm)
+    return mustache.replace(REGEXP, (...args) => {
+      const expr = args[1].trim()
+      return expr
     })
   }
 }
@@ -99,9 +115,14 @@ class Compiler {
   compile(root) {
     const childNodes = root.childNodes
     Array.from(childNodes).forEach(child => {
+      /* 編譯 dom */
       if (compileUtil.isElementNode(child)) {
+        // console.log('node child---',child);
         this.compileElementNode(child)
-      } else {
+      } 
+      /* 編譯 {{ }} */
+      else if (compileUtil.isMustacheSyntax(child)) {
+        // console.log('text child---',child);
         this.compileMustacheNode(child)
       }
       // 元素內若還有子元素則執行遞迴編譯
@@ -110,16 +131,16 @@ class Compiler {
   }
   /* 編譯 {{ }} 文本 */
   compileMustacheNode(node) {
-    const nodeContent = node.textContent.trim()
-    const value = compileUtil.getMustacheValue(nodeContent, this.vm)
-    updater.text(node, value)
+    const mustache = node.textContent.trim()
+    const expr = compileUtil.getMustacheExpr(mustache)
+    compileStrategies['text'](node, expr, this.vm)
   }
   /* 編譯 dom */
   compileElementNode(node) {
     const attrs = { ...node.attributes } // 取 child node 的 attributes
     for (const key of Object.keys(attrs)) {
       const { name, value } = attrs[key] // v-on:click="data.text" -> { name: 'v-on:click', value: 'data.text' }
-      const [directive, eventOrAttr] = name.split(':') // 1. v-on:click -> ['v-on', 'click']  2. :src="data" -> [undefined, 'src']
+      const [directive, eventOrAttr] = name.split(':') // 1. v-on:click -> ['v-on', 'click']  2. :src -> [undefined, 'src']
       // 處理 v-
       if (compileUtil.isDirective(directive)) {
         const strategy = compileUtil.getStrategyKey(directive)
@@ -132,7 +153,7 @@ class Compiler {
         const [, eventName] = name.split('@')
         compileStrategies['on'](node, value, this.vm, eventName)
       }
-      // v-bind alias 處理, 如 :src="data"
+      // v-bind alias 處理, 如 :src
       else if (compileUtil.isBindAlias(name)) {
         compileStrategies['bind'](node, value, this.vm, eventOrAttr)
       }
@@ -161,6 +182,23 @@ class Vue {
       new Observer(this.$data)
       // 模板語法編譯器
       new Compiler(this.$el, this)
+      // 代理模式，將 vm.$data 內所有資料代理至 vm 物件第一層
+      // 可使用簡短的 vm.somData 代替 vm.$data.someData 來訪問值。
+      this.dataProxy(this.$data)
     }
+  }
+  dataProxy(data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        configurable: false,
+        enumerable: true,
+        get() {
+          return data[key]
+        },
+        set(newValue) {
+          data[key] = newValue
+        }
+      })
+    })
   }
 }
